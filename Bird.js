f874cb86a8fff6387cf508d2b7c2119380314f71
Neev901 <legend.neev@gmail.com>
2020-08-29 11:55:37 +0530

    Add trajectory to the bird's movement

--- Bird.js
+++ Bird.js
@@ -2,12 +2,25 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.dot= loadImage("sprites/smoke.png")
+    this.birdpos= [];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
 
+    
+
     super.display();
+    if(this.body.position.x>200 && this.body.velocity.x>10){
+      var capturepos= [this.body.position.x, this.body.position.y]; 
+      this.birdpos.push(capturepos);
+    }
+      for(var count=0; count < this.birdpos.length; count++){
+        console.log(count)
+        image(this.dot, this.birdpos[count][0], this.birdpos[count][1])
+    }
   }
+
 }
